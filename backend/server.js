11ff087d346efb52f26c51b8b5823235766a5f1a
ThePeet25 <pbuythiy25@gmail.com>
2025-02-04 22:23:36 +0700

    version 0 add backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+//call express
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+})
+
+app.listen(PORT, () => {
+    console.log('server run on port:', PORT);
+})
