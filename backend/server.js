e2c817b1fc8e77b959e48678c9abbcf338ba7ec7
ThePeet25 <pbuythiy25@gmail.com>
2025-02-04 23:46:44 +0700

    add api for get user

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,20 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("hello world");
 })
 
 app.get('/api', (req, res) => {
     res.send("ky");
+})
+
+app.post('/api/user', (req, res) => {
+    const result = req.body
+    console.log(result);
+    res.send("I got this massage");
 })
 
 app.listen(PORT, () => {
