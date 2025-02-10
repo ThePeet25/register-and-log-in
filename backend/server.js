c0a14b13ece2109bcbd49074196d2c928590dcda
ThePeet25 <pbuythiy25@gmail.com>
2025-02-11 00:18:04 +0700

    return accesstoken to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
+//import .env file
+require('dotenv').config();
 //call express
 const express = require('express');
 //import connecting database
 const pool = require('./db');
 //import bcrypt hash function
 const bcrypt = require('bcrypt');
+//import jwt token use for authentication
+const jwt = require('jsonwebtoken');
+//import cors for accecpt origin to API
+const cors = require("cors");
+//import cookie parser for read data in cookies
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = 3001;
 
 //for read body
 app.use(express.json());
+//for read cookies
+app.use(cookieParser());
+//accecpt origin api
+app.use(cors({
+    credential: true,
+    origin: ['http://localhost:5173']
+}));
 
 //api for register
 app.post('/api/register', async (req, res) => {
@@ -65,10 +80,13 @@ app.get('/getall', async (req, res) => {
 
 //api for login 
 app.post('/api/login', async (req, res) => {
+    //get email and password from req
     const { email, password } = req.body
     try{
+        //query data from database
         const results = await pool.query(`select * from users where email = '${email}'`);
         const userData = results.rows[0]
+        //check password
         const match = await bcrypt.compare(password, userData.password)
         if(!match){
             res.status(400).json({
@@ -77,8 +95,24 @@ app.post('/api/login', async (req, res) => {
             //stop to use this function
             return false
         }
+        //secret for verify signature
+        const secret = process.env.JWT_SECRET;
+        //create jwt payload:{ email, role }, algorithm: HS512, expires in 1h
+        //payload: only identity user. not sensitive data!!!
+        const token = jwt.sign({ email, role: 'member' }, secret, { algorithm: "HS512", expiresIn: "1h" } );
+        //crate cookie for store token
+        res.cookie("token", token, {
+            //cookie expires in 30k ms
+            maxAge: 300000,
+            secure: true,
+            httpOnly: true,
+            //host ไม่จำเป็นต้องเป็นตัวเดียวกัน
+            sameSite: "none"
+        })
         console.log('login success')
-        res.json(userData)
+        res.json({
+            message: "login success"
+        })
     } catch(err) {
         console.log("err", err);
         res.status(401).json({
@@ -87,6 +121,39 @@ app.post('/api/login', async (req, res) => {
         })
     }
     
+});
+
+//api for authorization
+app.get('/api/user', async (req, res) => {
+    const { email } = req.body
+    const secret = process.env.JWT_SECRET
+    try {
+        //get token from cookies
+        const authToken = req.cookies.token
+        //const authHeader = req.headers['authorization'];
+        // let authToken = '';
+        // if (authHeader) {
+        //     authToken = authHeader.split(' ')[1];
+        // }
+        const user = jwt.verify(authToken, secret);
+        //user authorization pass
+        //recheck
+        const check = await pool.query(`select * from users where email = '${email}'`);
+        if (!check.rows[0]) {
+            throw { message: "user not found" }
+        }
+        const results = await pool.query(`select * from users where email = '${email}'`);
+        const userData = results.rows[0]
+        res.json({
+            userData
+        })
+    } catch(err) {
+        console.log("err", err);
+        res.status(403).json({
+            message: 'authentication fail',
+            err
+        })
+    }
 });
 
 app.listen(PORT, () => {
